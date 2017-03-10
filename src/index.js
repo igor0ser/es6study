2d4f0e575fe98e276fac203b4b8179904749ac4d
Igor Sergiienko <igor.sergiienko@racingpost.com>
2017-03-10 14:52:55 +0200

    ready for work

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import './main.css';
+import data from './data.json';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+console.log('Hello, ES6!');
+console.log(data);
+
